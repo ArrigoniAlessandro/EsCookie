0a303ea0b17955182b3e4ffa1585abc3e71111d3
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2022-03-10 13:04:17 +0100

    Create Cookie

---
+++ Cookie.php
@@ -0,0 +1,16 @@
+<?php
+echo ('<form action = "index.php" method = "POST">
+<p></p>login: <br>
+<input type = "text" name = "login"> </p>
+<p></p>password:<br>
+<input type = "text" name = "psw"></p>
+</p>
+<input type = "submit" name = "setCookie" value = "setta cookie"
+</form>');
+
+if(!empty($_POST)){
+    setcookie("a",$_POST["login"]."".$_POST["psw"]);
+}
+echo ("<h3>".$_COOKIE["a"]."<h3>");
+
+?>
