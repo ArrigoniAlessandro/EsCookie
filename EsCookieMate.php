1706d760906653c64f023cef12d6cb6886759a42
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2022-03-10 13:40:40 +0100

    pushata tattica contro adriano

--- EsCookieMate.php
+++ EsCookieMate.php
@@ -2,7 +2,7 @@
 <?php
 echo "
 <h1>Es Cookie</h1>
-<form method = post action = index.php>
+<form method = post action = EsCookieMate.php>
 inserisci un numero: <input type = number id = n name = numero></br>
 <input type = submit value = invia>
 <input type = submit value = resetta id = a name = a>
@@ -28,14 +28,14 @@ $array = explode("-", $cok);
 echo "totale: ".count($array);
 $indice = 0;
 for ($i = 0; $i<count($array); $i++) { 
-$indice = $indice + $array[$i];
+$numero = $numero + $array[$i];
 }
-echo "somma: ".$indice;
-echo "media: ".($indice)/count($array);
+echo "somma": $numero;
+echo "media":($numero)/count($array);
 }
 if(!empty($_POST['a'])){
-    $name= "numero"; //esenziale
-    $value= ""; //esenziale
+    $name= "numero"; 
+    $value= ""; 
     $expire=8600;
     $path="/";
     $domain="";
